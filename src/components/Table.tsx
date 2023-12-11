0af09ce6cb3248cbde0318f160989cfcff33ad4f
etleyden <etleyden@gmail.com>
2023-12-11 16:59:25 -0500

    Implemented Table Component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -19,24 +19,24 @@ export const Table: FC<TableProps> = ({data}) => {
     //For each item in col, make a new table heading:
 
 	var column_headings = (
-		<tr className="astra_table_heading">
-		{data.cols.map((heading, idx) => (<th className="astra_table_heading_cell" key={idx}>{heading}</th>))}
+		<tr className="astra_table_header_row">
+		{data.cols.map((heading, idx) => (<th className="astra_table_header_cell" key={idx}>{heading}</th>))}
 		</tr>);
 
 	var row_data =
 	data.rows.map((row: Array<any>, idx: number) => (
 		<tr className="astra_table_row" key={idx}>
-		{row.map((item: any, idx: number) => (<td className="astra_table_cell" key={idx}>{item}</td>))}
+		    {row.map((item: any, idx: number) => (<td className="astra_table_cell" key={idx}>{item}</td>))}
 		</tr>
 		));
 
 
-	console.log(column_headings);
-
 	return (
 		<table className="astra_table">
+            <thead className="astra_table_header">
+                {column_headings}
+            </thead>
 			<tbody className="astra_table_body">
-				{column_headings}
 				{row_data}
 			</tbody>
 		</table>
